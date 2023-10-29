5f706abc11b1f0840dfaa33ca54bddb16123c389
Laura Aubin <laura.aubin@shopify.com>
2023-10-29 17:17:43 -0400

    Show multiple boxes as well as toggling both the container display and the item display properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import React, {useState} from 'react';
 
-import {Card, Page, Select, VerticalStack, AppProvider} from '@shopify/polaris';
+import {
+  Box,
+  Card,
+  Checkbox,
+  Layout,
+  Page,
+  Select,
+  Text,
+  TextField,
+  AppProvider,
+  HorizontalStack,
+  VerticalStack,
+} from '@shopify/polaris';
 
 import './App.scss';
 
@@ -11,47 +23,111 @@ enum Display {
 }
 
 export default function App() {
-  const [color] = useState('teal');
+  const [boxDisplayProperty, setBoxDisplayProperty] = useState(Display.Block);
 
-  return (
-    <AppProvider i18n={[]}>
-      <Page title="">
-        <Card>
-          <Box />
-        </Card>
-      </Page>
-    </AppProvider>
+  const [boxCount, setBoxCount] = useState('2');
+  const [containerDisplayProperty, containerBoxDisplayProperty] = useState(
+    Display.Block
   );
-}
 
-function Box() {
-  const [boxCount, setBoxCount] = useState(1);
-  const [boxDisplayProperty, setBoxDisplayProperty] = useState(Display.Inline);
-
-  const renderBoxes = () => {
-    for (let i = 0; i < boxCount; i++) {
-      return (
-        <div className="Box" style={{display: boxDisplayProperty}}>
-          Box {boxCount}
-        </div>
-      );
-    }
+  const handleBoxCountChange = (value: string) => {
+    setBoxCount(value);
   };
 
-  const handleBoxDisplayOnChange = (value: Display) => {
+  const handleContainerDisplayChange = (value: Display) => {
+    containerBoxDisplayProperty(value);
+  };
+
+  const handleBoxDisplayChange = (value: Display) => {
     setBoxDisplayProperty(value);
   };
 
-  return (
-    <VerticalStack gap="2">
-      {renderBoxes()}
+  function RenderBoxes() {
+    const renderBoxes = () => {
+      const boxes = [];
 
-      <Select
-        label="Display"
-        value={boxDisplayProperty}
-        onChange={handleBoxDisplayOnChange}
-        options={[Display.Inline, Display.Block, Display.Flex]}
-      />
-    </VerticalStack>
+      for (let i = 0; i < Number(boxCount); i++) {
+        boxes.push(
+          <div
+            className="Box"
+            style={{display: boxDisplayProperty, width: 'fit-content'}}
+          >
+            Box {i + 1}
+          </div>
+        );
+      }
+      return boxes;
+    };
+
+    return (
+      <Layout.Section oneThird>
+        <Card padding="300">
+          <div style={{display: containerDisplayProperty}}>{renderBoxes()}</div>
+        </Card>
+      </Layout.Section>
+    );
+  }
+
+  return (
+    <AppProvider i18n={[]}>
+      <Page title="Flexy boxes playground" fullWidth>
+        <Layout>
+          <RenderBoxes />
+
+          <Layout.Section oneThird>
+            <VerticalStack gap="4">
+              <Card>
+                <VerticalStack gap="4">
+                  <Text as="h2" variant="headingMd">
+                    Box properties
+                  </Text>
+
+                  <Box width="100%">
+                    <Select
+                      label="Display"
+                      value={boxDisplayProperty}
+                      onChange={handleBoxDisplayChange}
+                      options={[Display.Inline, Display.Block, Display.Flex]}
+                      helpText="Default: block"
+                    />
+                  </Box>
+                </VerticalStack>
+              </Card>
+
+              <Card>
+                <VerticalStack gap="4">
+                  <Text as="h2" variant="headingMd">
+                    Container properties
+                  </Text>
+
+                  <HorizontalStack gap="4" wrap={false}>
+                    <Box maxWidth="150px">
+                      <TextField
+                        label="Box count"
+                        value={boxCount}
+                        onChange={handleBoxCountChange}
+                        autoComplete="off"
+                        type="number"
+                        selectTextOnFocus
+                      />
+                    </Box>
+
+                    <Box width="100%">
+                      <Select
+                        label="Display"
+                        value={containerDisplayProperty}
+                        onChange={handleContainerDisplayChange}
+                        options={[Display.Inline, Display.Block, Display.Flex]}
+                        helpText="Default: block"
+                      />
+                    </Box>
+                  </HorizontalStack>
+                </VerticalStack>
+              </Card>
+            </VerticalStack>
+          </Layout.Section>
+        </Layout>
+      </Page>
+    </AppProvider>
   );
 }
