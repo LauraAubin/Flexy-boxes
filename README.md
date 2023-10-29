71499609c09eb319a5dda6692c481e35a28fb91d
Laura Aubin <laura.aubin@shopify.com>
2023-10-29 16:32:27 -0400

    Allow the display property to be changed

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Flexy Boxes
 
-**Repurpose the [CSS Tricks Flexbox guide](https://css-tricks.com/snippets/css/a-guide-to-flexbox/) into a single-page playground**
+**Repurpose the [CSS Tricks Flexbox guide](https://css-tricks.com/snippets/css/a-guide-to-flexbox/) into a single-page playground designed with [Shopify Polaris](https://polaris.shopify.com/) 💜**
 
 >image here< 
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
 import React, {useState} from 'react';
+
+import {Card, Page, Select, VerticalStack, AppProvider} from '@shopify/polaris';
+
 import './App.scss';
 
-import {LegacyCard, Page, AppProvider} from '@shopify/polaris';
+enum Display {
+  Block = 'block',
+  Inline = 'inline',
+  Flex = 'flex',
+}
 
 export default function App() {
   const [color] = useState('teal');
 
   return (
     <AppProvider i18n={[]}>
-      <Page title="This is made using Create React App">
-        <LegacyCard title="This is a card thanks to Polaris ⭐️">
-          <LegacyCard.Section>
-            <div className={color}>
-              This text is {color.toLowerCase()} thanks to Sass 💅and Typescript
-              😄
-            </div>
-          </LegacyCard.Section>
-        </LegacyCard>
+      <Page title="">
+        <Card>
+          <Box />
+        </Card>
       </Page>
     </AppProvider>
+  );
+}
+
+function Box() {
+  const [boxCount, setBoxCount] = useState(1);
+  const [boxDisplayProperty, setBoxDisplayProperty] = useState(Display.Inline);
+
+  const renderBoxes = () => {
+    for (let i = 0; i < boxCount; i++) {
+      return (
+        <div className="Box" style={{display: boxDisplayProperty}}>
+          Box {boxCount}
+        </div>
+      );
+    }
+  };
+
+  const handleBoxDisplayOnChange = (value: Display) => {
+    setBoxDisplayProperty(value);
+  };
+
+  return (
+    <VerticalStack gap="2">
+      {renderBoxes()}
+
+      <Select
+        label="Display"
+        value={boxDisplayProperty}
+        onChange={handleBoxDisplayOnChange}
+        options={[Display.Inline, Display.Block, Display.Flex]}
+      />
+    </VerticalStack>
   );
 }
